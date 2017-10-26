d94c5fc919a3324ae2183e4dece8176392e80281
rchihu <rchihu@foundationmedicine.com>
2017-10-26 17:11:04 -0500

    pluck function problem solve

--- arrayHelpers/2-pluck-function-0.js
+++ arrayHelpers/2-pluck-function-0.js
@@ -2,6 +2,16 @@
 // and a string
 // representing a property name and return an  array containing that property
 // from each object.
-//
-var paints = [ { color: 'red' }, { color: 'blue' }, { color: 'yellow' }];
+
+var paints = [ 
+{ color: 'red', mx: true }, 
+{ color: 'blue', mx: false }, 
+{ color: 'yellow', mx: true }
+];
 var result = pluck(paints, 'color'); // should return ['red', 'yellow', 'blue'];
+
+function pluck (array, obj){
+  return array.map(array =>  array[obj])
+}
+
+console.log(result)
